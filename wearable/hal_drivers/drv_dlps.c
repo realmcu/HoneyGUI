829a9bc7616a75d59d93e07b8d9ac7749242b447
howie_wang <howie_wang@realsil.com.cn>
2023-05-26 14:51:30 +0800

    GUI: simple rtc driver
    Change-Id: I39e62ef29e0d34fabe8cac3481d0172773a91167
    Signed-off-by: howie_wang <howie_wang@realsil.com.cn>

--- wearable/hal_drivers/drv_dlps.c
+++ wearable/hal_drivers/drv_dlps.c
@@ -26,7 +26,7 @@ static dlps_slist_t drv_dlps_enter_slist =
 {
     .next = NULL,
 };
-static dlps_slist_t drv_dlps_cleck_slist =
+static dlps_slist_t drv_dlps_check_slist =
 {
     .next = NULL,
 };
@@ -44,7 +44,9 @@ static dlps_slist_t drv_dlps_wakeup_slist =
 RAM_FUNCTION
 static void System_Handler(void)
 {
-    //DBG_DIRECT("SYSTEM_HANDLER 0x%x", get_aon_wakeup_int());
+#ifdef RTL8772F
+    DBG_DIRECT("SYSTEM_HANDLER 0x%x", get_aon_wakeup_int());
+#endif
 
     dlps_slist_t *node;
     for (node = dlps_slist_first(&(drv_dlps_wakeup_slist)); node; node = dlps_slist_next(node))
@@ -53,7 +55,9 @@ static void System_Handler(void)
         p_item->dlps_cb();
     }
 
-    //clear_aon_wakeup_int();
+#ifdef RTL8772F
+    clear_aon_wakeup_int();
+#endif
 }
 
 #if (DLPS_EN == 1)
@@ -109,11 +113,11 @@ static void app_exit_dlps_config(void)
 RAM_FUNCTION
 static PMCheckResult app_dlps_check_cb(void)
 {
-
     dlps_slist_t *node;
-    for (node = dlps_slist_first(&(drv_dlps_exit_slist)); node; node = dlps_slist_next(node))
+    for (node = dlps_slist_first(&(drv_dlps_check_slist)); node; node = dlps_slist_next(node))
     {
         drv_dlps_cb_item_t *p_item = dlps_container_of(node, drv_dlps_cb_item_t, slist);
+        DBG_DIRECT("%s check fail! Module[%s]", __func__, p_item->name);
         return p_item->dlps_cb();
     }
 
@@ -146,19 +150,57 @@ void pwr_mgr_init(void)
 #endif
 }
 
-void drv_dlps_exit_cbacks_register(void *cbacks)
+void drv_dlps_exit_cbacks_register(const char *name, void *cbacks)
 {
     drv_dlps_cb_item_t *p_item = malloc(sizeof(drv_dlps_cb_item_t));
     if (p_item == NULL)
     {
         return;
     }
-
+    p_item->name = name;
     p_item->dlps_cb = cbacks;
 
 
     dlps_slist_append(&drv_dlps_exit_slist, &(p_item->slist));
+}
 
+void drv_dlps_enter_cbacks_register(const char *name, void *cbacks)
+{
+    drv_dlps_cb_item_t *p_item = malloc(sizeof(drv_dlps_cb_item_t));
+    if (p_item == NULL)
+    {
+        return;
+    }
+    p_item->name = name;
+    p_item->dlps_cb = cbacks;
+
+    dlps_slist_append(&drv_dlps_enter_slist, &(p_item->slist));
+}
+
+void drv_dlps_check_cbacks_register(const char *name, void *cbacks)
+{
+    drv_dlps_cb_item_t *p_item = malloc(sizeof(drv_dlps_cb_item_t));
+    if (p_item == NULL)
+    {
+        return;
+    }
+    p_item->name = name;
+    p_item->dlps_cb = cbacks;
+
+    dlps_slist_append(&drv_dlps_check_slist, &(p_item->slist));
+}
+
+void drv_dlps_wakeup_cbacks_register(const char *name, void *cbacks)
+{
+    drv_dlps_cb_item_t *p_item = malloc(sizeof(drv_dlps_cb_item_t));
+    if (p_item == NULL)
+    {
+        return;
+    }
+    p_item->name = name;
+    p_item->dlps_cb = cbacks;
+
+    dlps_slist_append(&drv_dlps_wakeup_slist, &(p_item->slist));
 }
 
 
