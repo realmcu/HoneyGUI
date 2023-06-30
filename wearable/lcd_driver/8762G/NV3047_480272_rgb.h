99d169323319ec43b8409fe59f9d695dd64179f7
luke_sun <luke_sun@realsil.com.cn>
2023-06-30 10:39:33 +0800

    GUI: add nv3047_480272
    Change-Id: I3edad1990f57103ff34cccd39306be28b63eaac1

---
+++ wearable/lcd_driver/8762G/NV3047_480272_rgb.h
@@ -0,0 +1,30 @@
+#ifndef _LCD_NV3047_480272_RGB_
+#define _LCD_NV3047_480272_RGB_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "stdint.h"
+
+
+#define NV3047_480272_LCD_WIDTH                   480
+#define NV3047_480272_LCD_HEIGHT                  272
+
+
+#define NV3047_DRV_PIXEL_BITS                  16
+
+
+
+void rtk_lcd_hal_init(void);
+void rtk_lcd_hal_update_framebuffer(uint8_t *p_buf, uint32_t size);
+void rtk_lcd_hal_set_window(uint16_t xStart, uint16_t yStart, uint16_t w, uint16_t h);
+void rtk_lcd_hal_start_transfer(uint8_t *buf, uint32_t len);
+void rtk_lcd_hal_transfer_done(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LCD_NV3047_480272_RGB_ */
