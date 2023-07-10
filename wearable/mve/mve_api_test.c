f302b1292744a6ad83f3324ea9f3955c7dd79bf2
boris_yue <boris_yue@realsil.com.cn>
2023-07-10 17:48:07 +0800

    GUI: add mve vaddq_m api test
    Change-Id: I0e9f69da2be657f8f4b58721a6e1d804bc00fae6

--- wearable/mve/mve_api_test.c
+++ wearable/mve/mve_api_test.c
@@ -6,9 +6,36 @@
 #include "trace.h"
 
 
+void vaddq_m_test()
+{
+    uint8x16_t inactive;
+    uint8x16_t a, b;
+
+    mve_pred16_t p = 0x02;
+
+    uint8_t a_array[16] = {1, 2, 3};
+    uint8_t b_array[16] = {4, 5, 6};
+    uint8_t inactive_array[16] = {0xa, 0xa, 0xa};
+    uint8_t ret_array[16] = {0};
+
+    a = vld1q(a_array);
+    b = vld1q(b_array);
+    inactive = vld1q(inactive_array);
+
+    uint8x16_t ret = vaddq_u8(a, b);
+    vstrbq(ret_array, ret);
+    DBG_DIRECT("vaddq_u8 test ret_array 0x%x 0x%x 0x%x 0x%x", ret_array[0], ret_array[1], ret_array[2],
+               ret_array[3]);
+
+    ret = vaddq_m_u8(inactive, a, b, p);
+    vstrbq(ret_array, ret);
+    DBG_DIRECT("vaddq_m_u8 test ret_array 0x%x 0x%x 0x%x 0x%x", ret_array[0], ret_array[1],
+               ret_array[2], ret_array[3]);
+}
+
 void mve_api_test()
 {
-    rtc_init();
+    vaddq_m_test();
 
 }
 
