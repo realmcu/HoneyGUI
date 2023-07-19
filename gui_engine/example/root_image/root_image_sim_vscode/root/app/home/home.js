f0caa4e3d37919c3c9a605bd117f4776cc0e5fc8
tritonyu <triton_yu@realsil.com.cn>
2023-07-19 17:21:57 +0800

    GUI: add home demo lamp js
    Change-Id: I80641e4cdcfd2bf84ce1c25b5dd4968ab516a967

--- gui_engine/example/root_image/root_image_sim_vscode/root/app/home/home.js
+++ gui_engine/example/root_image/root_image_sim_vscode/root/app/home/home.js
@@ -5,38 +5,91 @@ console.log('enter box3')
 var P1_1 = 9
 var P1_0 = 8
 var P2_5 = 21
+var lamp1 = P1_1
+var lamp2 = P1_0
+var lamp3 = P2_5
+var lamp4 = P1_1
+var lamp5 = P1_0
+var lamp6 = P2_5
 var LED1 = new Gpio(P1_0, 'out');
 var LED2 = new Gpio(P1_1, 'out');
 var LED3 = new Gpio(P2_5, 'out');
 console.log(LED1.gpio+';'+LED1.direction)
-LED1.writeSync(0)
-LED2.writeSync(0)
-LED3.writeSync(1)
-/*
-sw.getElementById('P1_0')
-function swOnOnFunc(params) {
-    console.log('swOnOnFunc')
+
+
+function led1OnFunc(params) {
+    console.log('led1OnFunc')
     LED1.writeSync(0)
 }
-sw.onOn(swOnOnFunc)
-
-function swOnOffFunc(params) {
-    console.log('swOnOffFunc')
+function led1OffFunc(params) {
+    console.log('led1OffFunc')
     LED1.writeSync(1)
 }
-sw.onOff(swOnOffFunc)
-
-sw.getElementById('P2_5')
-function swOnOnFunc2(params) {
-    console.log('swOnOnFunc2')
+function led2OnFunc(params) {
+    console.log('led2OnFunc')
+    LED2.writeSync(0)
+}
+function led2OffFunc(params) {
+    console.log('led2OffFunc')
+    LED2.writeSync(1)
+}
+function led3OnFunc(params) {
+    console.log('led3OnFunc')
     LED3.writeSync(1)
 }
-sw.onOn(swOnOnFunc2)
-
-function swOnOffFunc2(params) {
-    console.log('swOnOffFunc2')
+function led3OffFunc(params) {
+    console.log('led3OffFunc')
     LED3.writeSync(0)
 }
-sw.onOff(swOnOffFunc2)
-*/
+function lampSwitchOnFunc(params) {
+    console.log('lampSwitchOnFunc')
+    sw.getElementById('Kitchen')
+    sw.turnOn()
+    sw.getElementById('Parlor')
+    sw.turnOn()
+    sw.getElementById('porch')
+    sw.turnOn()   
+    sw.getElementById('bedroom')
+    sw.turnOn()
+    sw.getElementById('bedroom2')
+    sw.turnOn()
+    sw.getElementById('bedroom3')
+    sw.turnOn()       
+}
+function lampSwitchOffFunc(params) {
+    console.log('lampSwitchOffFunc')
+    sw.getElementById('Kitchen')
+    sw.turnOff()
+    sw.getElementById('Parlor')
+    sw.turnOff()
+    sw.getElementById('porch')
+    sw.turnOff()   
+    sw.getElementById('bedroom')
+    sw.turnOff()
+    sw.getElementById('bedroom2')
+    sw.turnOff()
+    sw.getElementById('bedroom3')
+    sw.turnOff()  
+}
+sw.getElementById('Kitchen')
+sw.onOn(led1OnFunc)
+sw.onOff(led1OffFunc)
+sw.getElementById('Parlor')
+sw.onOn(led2OnFunc)
+sw.onOff(led2OffFunc)
+sw.getElementById('bedroom')
+sw.onOn(led3OnFunc)
+sw.onOff(led3OffFunc)
+sw.getElementById('bedroom2')
+sw.onOn(led1OnFunc)
+sw.onOff(led1OffFunc)
+sw.getElementById('bedroom3')
+sw.onOn(led2OnFunc)
+sw.onOff(led2OffFunc)
+sw.getElementById('porch')
+sw.onOn(led3OnFunc)
+sw.onOff(led3OffFunc)
+sw.getElementById('lampSwitch')
+sw.onOn(lampSwitchOnFunc)
+sw.onOff(lampSwitchOffFunc)
 
