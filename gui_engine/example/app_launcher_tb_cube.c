bbd09eab5b1f1e33c6c4eae5584a8aa6a5e7b673
howie_wang <howie_wang@realsil.com.cn>
2023-08-01 17:24:58 +0800

    GUI: Set cube img to 64*64
    Change-Id: I0dc29c418bcfacde2b7918b1afb0aa73be213c64
    Signed-off-by: howie_wang <howie_wang@realsil.com.cn>

--- gui_engine/example/app_launcher_tb_cube.c
+++ gui_engine/example/app_launcher_tb_cube.c
@@ -7,10 +7,22 @@
 
 void page_tb_cube(void *parent)
 {
-    void *array[] = {C1_BIN, C2_BIN, C3_BIN, C4_BIN, C5_BIN, C6_BIN};
-    gui_cube_t *cube = gui_cube_create(parent, "cube", array, 0, 0, 454, 454);
-    gui_cube_set_size(cube, 50);
-    gui_cube_set_center(cube, 50, 50);
+    uint8_t *array_flash[] = {A1_BIN, A2_BIN, A3_BIN, A4_BIN, A5_BIN, A6_BIN};
+
+    uint8_t *array[6];
+
+#define CUBE_IMG_LEN (64*64*2 + 8)
+
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        array[i] = gui_malloc(CUBE_IMG_LEN + 64 + 8);
+        array[i] = array[i] + 64 - (int)array[i] % 64 - 8;
+        memcpy(array[i], array_flash[i], CUBE_IMG_LEN);
+    }
+
+    gui_cube_t *cube0 = gui_cube_create(parent, "cube", array, 0, 0, 454, 454);
+    gui_cube_set_size(cube0, 50);
+    gui_cube_set_center(cube0, 50, 50);
 
     gui_cube_t *cube1 = gui_cube_create(parent, "cube", array, 0, 0, 454, 454);
     gui_cube_set_size(cube1, 50);
