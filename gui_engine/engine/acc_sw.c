2bf461b803c68b46887547462c794e54f20e78aa
wenjing_jiang <wenjing_jiang@realsil.com.cn>
2023-08-24 19:03:14 +0800

    GUI: Add rle
    Change-Id: I4b64f1b13061e4c678dcfc886c360500446de692

--- gui_engine/engine/acc_sw.c
+++ gui_engine/engine/acc_sw.c
@@ -11,9 +11,7 @@
 #include "arm_math.h"
 #endif
 
-
-
-static void gui_memset16(uint16_t *addr, uint16_t pixel, uint32_t len)
+static void gui_memset16(uint16_t *addr, uint16_t pixel, uint32_t len) //rgb565
 {
 #if defined ( __CC_ARM )
     arm_fill_q15(pixel, (int16_t *)addr, len);
@@ -25,7 +23,18 @@ static void gui_memset16(uint16_t *addr, uint16_t pixel, uint32_t len)
     }
 #endif
 }
-
+static void gui_memset32(uint32_t *addr, uint32_t pixel, uint32_t len)  //argb8888
+{
+#if defined ( __CC_ARM )
+    arm_fill_q31(pixel, (int32_t *)addr, len);
+#endif
+#if defined(_MSC_VER) || (defined(__GNUC__))
+    for (uint32_t i = 0; i < len; i++)
+    {
+        addr[i] = pixel;
+    }
+#endif
+}
 typedef struct rtzip_file_header
 {
     struct
@@ -46,17 +55,21 @@ typedef struct rtzip_file
     uint32_t compressed_addr[1024];
 
 } rtzip_file_t;
-
 #pragma pack(1)
-typedef struct rtzip_node
+
+typedef struct rtzip_rgb565_node
 {
     uint8_t len;
-    uint16_t pixel16;
-} rtzip_node_t;
+    uint16_t pixel16;  //rgb565
+} rtzip_rgb565_node_t;
+typedef struct rtzip_argb8888_node
+{
+    uint8_t len;
+    uint32_t pixel32;    //argb8888
+} rtzip_argb8888_node_t;
 #pragma pack()
 
-
-static void uncompressed_rle(rtzip_file_t *file, uint32_t line,  uint8_t *buf)
+static void uncompressed_rle_rgb565(rtzip_file_t *file, uint32_t line,  uint8_t *buf)
 {
     //rtzip_file_header_t *header = (rtzip_file_header_t *)file;
     uint32_t start = (uint32_t)file + file->compressed_addr[line];
@@ -65,20 +78,30 @@ static void uncompressed_rle(rtzip_file_t *file, uint32_t line,  uint8_t *buf)
 
     for (uint32_t addr = start; addr < end;)
     {
-        rtzip_node_t *node = (rtzip_node_t *)addr;
+        rtzip_rgb565_node_t *node = (rtzip_rgb565_node_t *)addr;
         gui_memset16(linebuf, node->pixel16, node->len);
 
-        addr = addr + sizeof(rtzip_node_t);
+        addr = addr + sizeof(rtzip_rgb565_node_t);
+        linebuf = linebuf + node->len;
+    }
+}
+static void uncompressed_rle_argb8888(rtzip_file_t *file, uint32_t line,  uint8_t *buf)
+{
+    //rtzip_file_header_t *header = (rtzip_file_header_t *)file;
+    uint32_t start = (uint32_t)file + file->compressed_addr[line];
+    uint32_t end = (uint32_t)file + file->compressed_addr[line + 1];
+    uint32_t *linebuf = (uint32_t *)buf;
+
+    for (uint32_t addr = start; addr < end;)
+    {
+        rtzip_argb8888_node_t *node = (rtzip_argb8888_node_t *)addr;
+        gui_memset32(linebuf, node->pixel32, node->len);
+
+        addr = addr + sizeof(rtzip_argb8888_node_t);
         linebuf = linebuf + node->len;
     }
 }
 
-static void rle_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
-                                struct rtgui_rect *rect)
-{
-    gui_log("todo for this line = %d \n", __LINE__);
-    return;
-}
 static void rle_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
                               struct rtgui_rect *rect)
 {
@@ -97,36 +120,157 @@ static void rle_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
         return;
     }
 
-    uint8_t source_bytes_per_pixel = 2;
-    uint8_t line_buf[source_bytes_per_pixel * image_w];
-
+    //uint8_t dc_bytes_per_pixel = dc->bit_depth >> 3;
     uint32_t image_off = sizeof(struct gui_rgb_data_head) + (uint32_t)(image->data);
-
+    //uint8_t *head = (uint8_t *)((uint8_t *)(image->data) + sizeof(struct gui_rgb_data_head));
+    uint8_t img_type = *((uint8_t *)image_off);
     rtzip_file_t *file = (rtzip_file_t *)image_off;
 
-    for (uint32_t i = y_start; i < y_end; i++)
+    if (img_type == 4)
     {
-        int write_off = (i - dc->section.y1) * dc->fb_width ;
+        uint8_t source_bytes_per_pixel = 2;
+        uint8_t line_buf[source_bytes_per_pixel * image_w];
 
-        int line = i - image_y;
-
-        uncompressed_rle(file, line, line_buf);
-
-        int read_off = (int)line_buf - source_bytes_per_pixel * x_start;
-
-        uint16_t *writebuf = (uint16_t *)dc->frame_buf;
-
-        for (uint32_t j = x_start; j < x_end; j++)
+        for (uint32_t i = y_start; i < y_end; i++)
         {
-            if (*((uint16_t *)read_off + j) != 0)
+            int write_off = (i - dc->section.y1) * dc->fb_width ;
+
+            int line = i - image_y;
+
+            uncompressed_rle_rgb565(file, line, line_buf);
+
+            int read_off = (int)line_buf - source_bytes_per_pixel * x_start;
+
+            uint16_t *writebuf = (uint16_t *)dc->frame_buf;
+
+            for (uint32_t j = x_start; j < x_end; j++)
             {
-                writebuf[write_off + j] = (*((uint16_t *)read_off + j));
+                if (*((uint16_t *)read_off + j) != 0)
+                {
+                    writebuf[write_off + j] = (*((uint16_t *)read_off + j));
+                }
+            }
+        }
+    }
+    else if (img_type == 132) //argb8888_2_rgb565
+    {
+        uint8_t source_bytes_per_pixel = 4;
+        uint8_t line_buf[source_bytes_per_pixel * image_w];
+        for (uint32_t i = y_start; i < y_end; i++)
+        {
+            int write_off = (i - dc->section.y1) * dc->fb_width ;
+
+            int line = i - image_y;
+            uncompressed_rle_argb8888(file, line, line_buf);
+            int read_off = (int)line_buf - source_bytes_per_pixel * x_start;
+
+            uint16_t *writebuf = (uint16_t *) dc->frame_buf;
+
+            for (uint32_t j = x_start; j < x_end; j++)
+            {
+                uint8_t *pixel = (uint8_t *)(read_off + j * source_bytes_per_pixel);
+                if (pixel != 0)
+                {
+                    writebuf[write_off + j] = (uint16_t)(((pixel[2]) >> 3) << 11) + (uint16_t)(((pixel[1]) >> 2) << 5)
+                                              + (uint16_t)(((pixel[0]) >> 3)); //RGB  565
+                }
             }
         }
     }
 }
+static void rle_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
+                                struct rtgui_rect *rect)
+{
+    int image_x = rect->x1;
+    int image_y = rect->y1;
+    int image_w = image->img_w;
+    int image_h = image->img_h;
 
+    int x_start = _UI_MAX(image_x, 0);
+    int x_end = _UI_MIN(image_x + image_w, dc->fb_width);
+    int y_start = _UI_MAX(dc->section.y1, image_y);
+    int y_end = _UI_MIN(dc->section.y2, image_y + image_h);
 
+    if ((x_start >= x_end) || (y_start >= y_end))
+    {
+        return;
+    }
+    uint32_t image_off = sizeof(struct gui_rgb_data_head) + (uint32_t)(image->data);
+    uint8_t img_type = *((uint8_t *)image_off);
+    uint8_t dc_bytes_per_pixel = dc->bit_depth >> 3;
+
+    if (img_type == 132)
+    {
+        uint8_t source_bytes_per_pixel = 4;
+        uint8_t line_buf[source_bytes_per_pixel * image_w];
+        rtzip_file_t *file = (rtzip_file_t *)image_off;
+        for (uint32_t i = y_start; i < y_end; i++)
+        {
+            int write_off = (i - dc->section.y1) * dc->fb_width ;
+            int line = i - image_y;
+            uncompressed_rle_argb8888(file, line, line_buf);
+            int read_off = (int)line_buf - source_bytes_per_pixel * x_start;
+            uint8_t *writebuf = dc->frame_buf;
+            uint8_t *pixel;
+            for (uint32_t j = x_start; j < x_end; j++)
+            {
+                pixel = (uint8_t *)(read_off + j * source_bytes_per_pixel);
+                switch (image->opacity_value)
+                {
+                case 0:
+                    break;
+                case 255:
+                    {
+                        writebuf[(write_off + j) * dc_bytes_per_pixel    ] = pixel[0]; //B
+                        writebuf[(write_off + j) * dc_bytes_per_pixel + 1] = pixel[1]; //G
+                        writebuf[(write_off + j) * dc_bytes_per_pixel + 2] = pixel[2]; //R
+                        writebuf[(write_off + j) * dc_bytes_per_pixel + 3] = pixel[3]; //A
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+    else if (img_type == 4) //rgb565_2_argb8888
+    {
+        uint8_t source_bytes_per_pixel = 2;
+        uint8_t line_buf[source_bytes_per_pixel * image_w];
+        rtzip_file_t *file = (rtzip_file_t *)image_off;
+        for (uint32_t i = y_start; i < y_end; i++)
+        {
+            int write_off = (i - dc->section.y1) * dc->fb_width ;
+            int line = i - image_y;
+            uncompressed_rle_rgb565(file, line, line_buf);
+            int read_off = (int)line_buf - source_bytes_per_pixel * x_start;
+            uint8_t *writebuf = dc->frame_buf;
+
+            for (uint32_t j = x_start; j < x_end; j++)
+            {
+                uint16_t pixel = *((uint16_t *)(read_off + j * source_bytes_per_pixel));
+                if (pixel != 0)
+                {
+                    writebuf[(write_off + j) * dc_bytes_per_pixel + 2] = (pixel >> 11) << 3;
+                    writebuf[(write_off + j) * dc_bytes_per_pixel + 1] = ((pixel & 0x07e0) >> 5) << 2;
+                    writebuf[(write_off + j) * dc_bytes_per_pixel] = (pixel & 0x001f) << 3;
+                }
+            }
+        }
+
+    }
+    return;
+}
+static void do_blending_argb8888_2_argb8888(uint8_t *target, gui_color_t color)
+{
+    gui_log("[GUI] TODO line = %d", __LINE__);
+    while (1);
+}
+static void do_blending_argb8888_2_rgb565(uint8_t *target, gui_color_t color)
+{
+    gui_log("[GUI] TODO line = %d", __LINE__);
+    while (1);
+}
 static void normal_blit_rgb565_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
                                         struct rtgui_rect *rect)
 {
@@ -266,17 +410,7 @@ static void normal_blit_rgb888_2_argb8888(draw_img_t *image, struct gui_dispdev 
     }
 }
 
-static void do_blending_argb8888_2_argb8888(uint8_t *target, gui_color_t color)
-{
-    gui_log("[GUI] TODO line = %d", __LINE__);
-    while (1);
-}
 
-static void do_blending_argb8888_2_rgb565(uint8_t *target, gui_color_t color)
-{
-    gui_log("[GUI] TODO line = %d", __LINE__);
-    while (1);
-}
 
 static void normal_blit_rgba8888_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
                                             struct rtgui_rect *rect)
