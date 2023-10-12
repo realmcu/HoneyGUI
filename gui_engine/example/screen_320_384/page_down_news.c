405f4fe71b48f14b8cefae55e0d5270ccb830983
wenjing_jiang <wenjing_jiang@realsil.com.cn>
2023-10-12 15:30:34 +0800

    GUI: modify down curtain
    Change-Id: I09145c9e18a1ad20b84916d6851ab174d75ef983

--- gui_engine/example/screen_320_384/page_down_news.c
+++ gui_engine/example/screen_320_384/page_down_news.c
@@ -15,10 +15,10 @@ void page_down_message_health(void *parent)
                                                                      10, 0, 0, 0);
     gui_magic_img_t *health_watch =  gui_magic_img_create_from_mem(parent, "card_time",
                                                                    CARD_TIME_BIN,
-                                                                   170, -138, 0, 0);
+                                                                   170, -148, 0, 0);
     gui_img_set_opacity(health_watch, 50);
-    gui_magic_img_t *week =  gui_magic_img_create_from_mem(parent, "time", SUN_BIN, 25, -90, 0, 0);
-    gui_magic_img_t *data =  gui_magic_img_create_from_mem(parent, "data", DATA_BIN, 25, -37, 0, 0);
+    gui_magic_img_t *week =  gui_magic_img_create_from_mem(parent, "time", SUN_BIN, 15, -100, 0, 0);
+    gui_magic_img_t *data =  gui_magic_img_create_from_mem(parent, "data", DATA_BIN, 25, -47, 0, 0);
 }
 void page_down_message_music(void *parent)
 {
@@ -46,32 +46,60 @@ void page_down_message_mult(void *parent)
                                                                    10, 0, 0, 0);
     gui_magic_img_t *all_app =  gui_magic_img_create_from_mem(parent, "close",
                                                               DYNAMIC_ISLAND_260_66_BIN,
-                                                              30, 156, 0, 0);
-    gui_magic_img_t *menu = gui_magic_img_create_from_mem(parent, "menu", CARD_LIST_BIN, 140, 170, 0,
+                                                              30, 256, 0, 0);
+    gui_magic_img_t *menu = gui_magic_img_create_from_mem(parent, "menu", CARD_LIST_BIN, 140, 270, 0,
                                                           0);
 }
-//void page_down_all(void *parent)
-//{
-//    gui_magic_img_t *all_app =  gui_magic_img_create_from_mem(parent, "close",
-//                                                              DYNAMIC_ISLAND_260_66_BIN,
-//                                                              30, 306, 0, 0);
-//    gui_magic_img_t *menu = gui_magic_img_create_from_mem(parent, "menu", CARD_LIST_BIN, 140, 320, 0,
-//                                                          0);
-//    //gui_img_set_opacity(close, 50);
-//}
+static gui_cardview_t *cv;
+static void done_cb(gui_curtainview_t *this)
+{
+    if (this->cur_curtain == CURTAIN_DOWN)
+    {
+        gui_log("CURTAIN_DOWN done!!! \n");
+        cv->mute = false;
+    }
+    if (this->cur_curtain == CURTAIN_MIDDLE)
+    {
+        gui_log("CURTAIN_DOWN Reset!!! \n");
+        cv->mute = true;
+        cv->release_y = 0;
+        cv->remain_y = 0;
+    }
+
+}
+static void cv_status_cb(gui_cardview_t *this)
+{
+    //gui_log("cv release value = %d \n", this->release_y);
+
+    gui_curtainview_t *curtainview = (gui_curtainview_t *)this->base.parent->parent;
+
+    if (this->release_y != 0)
+    {
+        curtainview->mute = true;
+    }
+    else
+    {
+        curtainview->mute = false;
+    }
+}
 void  page_down_message_design(void *parent)
 {
     gui_magic_img_t *bg = gui_magic_img_create_from_mem(parent, "bg_up", RECT_320_384_BIN, 0, 0, 0, 0);
     gui_img_set_opacity(bg, 128);
-    gui_cardview_t *cd =  gui_cardview_create(parent, "cardview",    0, 0, 0, 0);
-    gui_cardview_set_style(cd, STACKING);
 
-    gui_card_t *tb_health   =    gui_card_create(cd, "tb_health",  0, 0, 0, 0, 0, 0);
-    gui_card_t *tb_weather1 = gui_card_create(cd, "tb_weather1",   0, 0, 0, 0, 0, 1);
-    gui_card_t *tb_music    =  gui_card_create(cd, "tb_music",     0, 0, 0, 0, 0, 2);
-    gui_card_t *tb_weather2 =  gui_card_create(cd, "tb_weather2",  0, 0, 0, 0, 0, 3);
-    gui_card_t *tb_more     =      gui_card_create(cd, "tb_more",  0, 0, 0, 0, 0, 4);
-    gui_card_t *tb_non      =       gui_card_create(cd, "tb_non",  0, 0, 0, 0, 0, 5);
+    gui_curtain_t *ct_card = parent;
+    gui_curtainview_t *curtainview = (gui_curtainview_t *)ct_card->base.parent;
+    gui_curtainview_set_done_cb(curtainview, done_cb);
+
+    cv =  gui_cardview_create(parent, "cardview",    0, 0, 0, 0);
+    //gui_cardview_set_style(cd, STACKING);
+    gui_cardview_status_cb(cv, cv_status_cb);
+
+    gui_card_t *tb_more     =      gui_card_create(cv, "tb_more",  0, 50, 0, 0, 0, 4);
+    gui_card_t *tb_weather2 =  gui_card_create(cv, "tb_weather2",  0, 50, 0, 0, 0, 3);
+    gui_card_t *tb_music    =  gui_card_create(cv, "tb_music",     0, 50, 0, 0, 0, 2);
+    gui_card_t *tb_weather1 = gui_card_create(cv, "tb_weather1",   0, 50, 0, 0, 0, 1);
+    gui_card_t *tb_health   =  gui_card_create(cv, "tb_health",  0, 210, 0, 0, 0, 0);
 
     page_down_message_health(tb_health);
     page_down_message_weather1(tb_weather1);
