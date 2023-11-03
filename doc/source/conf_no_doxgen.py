dd6c7dc6bf44fec2a29fcbf46f6cf8859e818dd2
howie_wang <howie_wang@realsil.com.cn>
2023-11-03 19:16:51 +0800

    GUI: Add no doxgen conf py
    Signed-off-by: howie_wang <howie_wang@realsil.com.cn>
    Change-Id: Ide40a87da872ccd644f71314516128b8d6445448

---
+++ doc/source/conf_no_doxgen.py
@@ -0,0 +1,150 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+project = 'RTKIOT GUI'
+author = 'RTKIOT GUI'
+release = 'v0.0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["breathe", 
+              "myst_parser", 
+              "sphinx_rtd_theme"]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# The default language to highlight source code in. The default is 'python'.
+# The value should be a valid Pygments lexer name, see Showing code examples for more details.
+
+
+highlight_language = 'c'
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md']
+
+source_encoding = "utf-8"
+
+# The master toctree document.
+master_doc = "index"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+html_static_path = ['_static']
+
+html_logo = './_static/image/bbpro-h55px.jpg'
+html_favicon = "./_static/image/favicon.ico"
+
+html_show_sourcelink = False
+
+html_theme_options = {
+    'collapse_navigation' : False,
+    'logo_only': True,
+}
+
+html_js_files = [
+    'js/custom.js'
+]
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+
+    'inputenc': '',
+    'utf8extra': '',
+    'classoptions': ',openany,oneside',
+    'babel': '\\usepackage{babel}',
+    'passoptionstopackages': r'''
+\PassOptionsToPackage{bookmarksdepth=5}{hyperref}% depth of pdf bookmarks
+''',
+    'preamble': r'''
+\usepackage{fontspec}
+\setmonofont{DejaVu Sans Mono}
+\usepackage{silence}
+\WarningsOff*
+''',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'BeeGUI.tex', 'BeeGUI Documentation',
+     'BeeGUI community', 'manual'),
+]
+
+
+# -- Options for manual page output ---------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'BeeGUI', 'BeeGUI Documentation',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output -------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'BeeGUI', 'BeeGUI Documentation',
+     author, 'Contributors of BeeGUI', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+# -- Options for Breathe ----------------------------------------------------
+
+# breathe_projects = {"HoneyGUI": os.path.join(os.path.dirname(__file__), r"..\doxyxml\xml")}
+# breathe_default_project = "HoneyGUI"
+# breathe_domain_by_extension = {"h" : "c"}
+# breathe_implementation_filename_extensions = ['.c', '.cc', '.cpp']
+
+
+# Example configuration for intersphinx: refer to the Python standard library.
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'enable_eval_rst': True,
+            'enable_auto_toc_tree': 'True',
+            }, True)
+    app.add_transform(AutoStructify)
+    app.add_css_file('css/custom.css')
+    app.add_css_file('css/fontawesome.min.css')
