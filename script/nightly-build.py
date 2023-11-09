0819790c9fd054fa6f470b3bd7547f0b69f52251
tracy_yan <tracy_yan@realsil.com.cn>
2023-11-09 17:54:57 +0800

    Script-Nightly: enable BUILD_USING_SCRIPT_AS_A_APP before nightly build
    Change-Id: I860f9429c3a756642285b6cd0306ee0eb183aa26

--- script/nightly-build.py
+++ script/nightly-build.py
@@ -29,6 +29,40 @@ from jenkins_base import JenkinsCheckBase
 from keil_build import *
 
 
+def change_or_revert_macros(repo, app_flag_path, update_type, macro_list, insert=False):
+    if update_type == "change" and not macro_list:
+        print("There is no macro to change, skip")
+        return
+
+    if update_type == 'change':
+        with open(app_flag_path, mode='r', newline='', errors='surrogateescape') as fd:
+            lines = fd.readlines()
+ 
+        with open(app_flag_path, mode='w+', newline='', errors='surrogateescape') as fd:
+            for macro, value, check_pattern in macro_list:
+                start_replace = False if check_pattern else True
+                pattern = r'^(\s*#define\s+{}\s+)(\d+)'.format(macro)
+                for i, line in enumerate(lines):
+                    if check_pattern and re.search(check_pattern, line):
+                        print("Find target definition, start change macro {}".format(macro))
+                        start_replace = True
+                    if start_replace:
+                        if insert:
+                            lines[i] = line + "#undef {}\n#define {}\t{}\n".format(macro, macro, value)
+                            start_replace = False
+                        else:
+                            if re.search(pattern, line):
+                                lines[i] = re.sub(pattern, lambda objs:objs.group(1)+value, line, count=1, flags=re.M)
+                                start_replace = False
+            fd.writelines(lines)
+        
+        print('Configure {} diff ->\n{}'.format(os.path.basename(app_flag_path), repo.git.diff(app_flag_path)), flush=True)
+        
+    elif update_type == 'revert':
+        repo.git.checkout('--', app_flag_path)
+        print('after revert {} diff ->\n{}'.format(os.path.basename(app_flag_path), repo.git.diff(app_flag_path)), flush=True)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='HoneyPatch CI build script')
     parser.add_argument('-c', '--chipType', choices=['guiEngine-test-chip'], help='Set the chip type')
@@ -40,6 +74,7 @@ if __name__ == '__main__':
     print("\n================ build {} ====================\n".format(chip_type), flush=True)
     keil_builder = SDKBuild(os.environ.get("manifest"), os.environ.get("HoneyRepo"), chip_type)
     print("call build {}".format(chip_type))
+    change_or_revert_macros(repo, "./keil_sim/menu_config.h", "change", [("BUILD_USING_SCRIPT_AS_A_APP", "", "BUILD_USING_SCRIPT_AS_A_APP")], True)
     os.chdir('./keil_sim')
     subprocess.check_call(["scons", "--target=mdk5"], universal_newlines=True, stderr=subprocess.STDOUT)
     os.chdir('./..')
