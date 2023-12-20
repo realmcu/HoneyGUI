3ee0a0976e22305538bf52263ba9fd779c5d648e
lizliu <liz_liu@realsil.com.cn>
2023-12-20 10:54:20 +0800

    GUI: Add watchface to rotate in real-time time
    [JIRA]BB2RD-2976
    
    [JIRA TYPE] Bug-Native-Minor
    
    [Introduced by] NA
    
    [Issue description]
    Add watchface to rotate in real-time time
    
    [Issue reproduction]
    NA
    
    [Cause]
    NA
    
    [Solution]
    Add watchface to rotate in real-time time
    
    [Validation] NA
    
    [Need Cherry Pick to] NA
    
    [Equivalent to] NA
    
    Change-Id: I2523849ff31ae7438476b16225099f01003debb6

--- gui_engine/example/screen_454_454_bb2plus_watch/gui_watchface/tab_watchface.c
+++ gui_engine/example/screen_454_454_bb2plus_watch/gui_watchface/tab_watchface.c
@@ -5,38 +5,45 @@
 #include <time.h>
 #include <stdio.h>
 #include "gui_text.h"
-//#include "os_timer.h"
+
+#define MY_PI   (180)
 
 void *gui_watchface_turn_around_timer = NULL;
 
 gui_img_t *img_clock = NULL;
 gui_curtainview_t *curtainview_menu = NULL;
-
 gui_img_t *watchface_hand;
 gui_img_t *watchface_hour;
 gui_img_t *watchface_minute;
 gui_img_t *watchface_second;
-float watchface_degree = 0;
-
-/* static void watchface_turn_around_timer_callback(void *pxTimer)
-{
-    watchface_degree += 0.1;
-} */
+float angle_hour;
+float angle_min;
+float angle_sec;
 
 static void img_animate_watchface_callback(void *obj)
 {
-    //os_timer_create(&gui_watchface_turn_around_timer, "watchface_turn_around_timer", 1, 1000, false, watchface_turn_around_timer_callback);
-    //os_timer_start(&gui_watchface_turn_around_timer);
-    watchface_degree += 0.0022;
+//Get the current real-time time and print it
+    struct tm *ptr;
+    time_t lt;
+    lt = time(NULL);
+    ptr = localtime(&lt);
+
+    uint8_t tm_sec = ptr->tm_sec;
+    uint8_t tm_min = ptr->tm_min;
+    uint8_t tm_hour = ptr->tm_hour;
+
+    angle_hour = (tm_hour % 12) * MY_PI / 6 + tm_min * MY_PI / 360;
+    angle_min  = tm_min * MY_PI / 30 + tm_sec * MY_PI / 1800;
+    angle_sec  = tm_sec * MY_PI / 30;
     gui_img_translate(watchface_hour, watchface_hour->base.w / 2, watchface_hour->base.h);
     gui_img_translate(watchface_minute, watchface_minute->base.w / 2, watchface_minute->base.h);
     gui_img_translate(watchface_second, watchface_second->base.w / 2, watchface_second->base.h);
 
-    gui_img_rotation(watchface_hour, watchface_degree / 12,  watchface_hour->base.w / 2,
+    gui_img_rotation(watchface_hour, angle_hour,  watchface_hour->base.w / 2,
                      watchface_hour->base.h);
-    gui_img_rotation(watchface_minute, watchface_degree, watchface_minute->base.w / 2,
+    gui_img_rotation(watchface_minute, angle_min, watchface_minute->base.w / 2,
                      watchface_minute->base.h);
-    gui_img_rotation(watchface_second, watchface_degree * 60, watchface_second->base.w / 2,
+    gui_img_rotation(watchface_second, angle_sec, watchface_second->base.w / 2,
                      watchface_second->base.h);
 }
 
@@ -51,20 +58,9 @@ void design_tab_watchface(void *parent)
                                                144);
     watchface_hand = gui_img_create_from_mem(parent, "hand", ICON_CLOCK_HAND_DOT_BIN, 218, 218, 0, 0);
 
-    gui_img_set_animate(watchface_second, 33, -1, img_animate_watchface_callback, NULL);
+    gui_img_set_animate(watchface_second, 1, -1, img_animate_watchface_callback, NULL);
 
     curtainview_menu = gui_curtainview_create(parent, "curtainview_menu", 0, 0, 454, 454);
     void design_curtain_menu(void *parent);
     design_curtain_menu(curtainview_menu);
-//Get the current real-time time and print it
-//    struct tm *ptr;
-//    time_t lt;
-//    lt = time(NULL);
-//    ptr = localtime(&lt);
-//    printf(asctime(ptr));
-
-//    int tm_sec = ptr->tm_sec;
-//    int tm_min = ptr->tm_min;
-//    int tm_hour = ptr->tm_hour;
-//    printf("time: %d:%d:%d \n", tm_hour, tm_min, tm_sec);
 }
