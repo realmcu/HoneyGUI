7b3f11505d6f38870c46e897db6d6d2b987098bb
wenjing_jiang <wenjing_jiang@realsil.com.cn>
2023-12-25 09:52:04 +0800

    GUI: rle sw modify opacity output rgb565 set opacity add bypass matrix
    [JIRA] BTSOCGUI-137
    
    Change-Id: Ifcf81029076e453120b7ff902a5bcdda532d7695

--- gui_engine/engine/acc_sw_rle.h
+++ gui_engine/engine/acc_sw_rle.h
@@ -21,19 +21,70 @@
 extern "C" {
 #endif
 
+typedef struct rtzip_file_header
+{
+    struct
+    {
+        uint8_t algorithm: 2;
+        uint8_t feature_1: 2;
+        uint8_t feature_2: 2;
+        uint8_t pixel_bytes: 2;
+    } algorithm_type;
+    uint8_t reserved[3];
+    uint32_t raw_pic_width;
+    uint32_t raw_pic_height;
+} rtzip_file_header_t;
+
+typedef struct rtzip_file
+{
+    rtzip_file_header_t header;
+    uint32_t compressed_addr[1024];
+
+} rtzip_file_t;
+#pragma pack(1)
+
+typedef struct rtzip_rgb565_node
+{
+    uint8_t len;
+    uint16_t pixel16;  //rgb565
+} rtzip_rgb565_node_t;
+typedef struct rtzip_rgb888_node
+{
+    uint8_t len;
+    uint8_t pixel_b;  //rgb888
+    uint8_t pixel_g;
+    uint8_t pixel_r;
+} rtzip_rgb888_node_t;
+typedef struct rtzip_argb8888_node
+{
+    uint8_t len;
+    uint32_t pixel32;    //argb8888
+} rtzip_argb8888_node_t;
+#pragma pack()
+
 void rle_bypass_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
+                              struct rtgui_rect *rect);
+void rle_bypass_blit_2_rgb888(draw_img_t *image, struct gui_dispdev *dc,
                               struct rtgui_rect *rect);
 void rle_bypass_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
                                 struct rtgui_rect *rect);
+void rle_bypass_matrix_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
+                                     struct rtgui_rect *rect);
+void rle_bypass_matrix_blit_2_rgb888(draw_img_t *image, struct gui_dispdev *dc,
+                                     struct rtgui_rect *rect);
+void rle_bypass_matrix_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
+                                       struct rtgui_rect *rect);
 void rle_filter_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
                               struct rtgui_rect *rect);
-
+void rle_filter_blit_2_rgb888(draw_img_t *image, struct gui_dispdev *dc,
+                              struct rtgui_rect *rect);
 void rle_filter_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
                                 struct rtgui_rect *rect);
 
 void rle_filter_matrix_blit_2_rgb565(draw_img_t *image, struct gui_dispdev *dc,
                                      struct rtgui_rect *rect);
-
+void rle_filter_matrix_blit_2_rgb888(draw_img_t *image, struct gui_dispdev *dc,
+                                     struct rtgui_rect *rect);
 void rle_filter_matrix_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
                                        struct rtgui_rect *rect);
 void rle_alpha_blend_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
@@ -43,6 +94,11 @@ void rle_alpha_matrix_blit_2_argb8888(draw_img_t *image, struct gui_dispdev *dc,
 void rle(draw_img_t *image, struct gui_dispdev *dc,
          struct rtgui_rect *rect);
 
+static void gui_memset16(uint16_t *addr, uint16_t pixel, uint32_t len);
+static void gui_memset32(uint32_t *addr, uint32_t pixel, uint32_t len);
+static void uncompressed_rle_rgb565(rtzip_file_t *file, uint32_t line,  uint8_t *buf);
+void uncompressed_rle_rgb888(rtzip_file_t *file, uint32_t line,  uint8_t *buf);
+void uncompressed_rle_argb8888(rtzip_file_t *file, uint32_t line,  uint8_t *buf);
 
 
 #ifdef __cplusplus
