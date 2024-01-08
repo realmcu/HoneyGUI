e7a7322b90d90b1397a2ad30526d6ae1c8642687
howie_wang <howie_wang@realsil.com.cn>
2024-01-08 16:53:54 +0800

    GUI: Add painter engine source code
    Change-Id: If6c31852bb04ac6ffe71ec083540cd582dbd4528

--- script/ignore_check_file.py
+++ script/ignore_check_file.py
@@ -1,16 +1,9 @@
 ignore_file_encoding = [
-    r"gui_engine/document/",
-    r"wearable/tuya-ble-sdk/",
-    r"wearable/database/",
-    r"wearable/vcxx/",
-    r"rt-thread/",
+    r"gui_engine/3rd/",
+    r"lvgl/",
     r"gui_engine/example/screen_454_454_bb2plus_watch"
 ]
 ignore_file_check = [
+    r"gui_engine/3rd/",
     r"lvgl/",
-    r"VGLite/",
-    r"wearable/tuya-ble-sdk/",
-    r"wearable/database/",
-    r"wearable/vcxx/",
-    r"rt-thread/",
 ]
