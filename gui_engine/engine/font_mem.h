9e09eb0dd557400bc3bff708e5dae16b3db026b4
luke_sun <luke_sun@realsil.com.cn>
2024-02-22 16:20:44 +0800

    GUI: add font width api
    [JIRA]BB2PLUSBUG-70
    
    Change-Id: Ib52eebd1972f5f76d77321d122b2dee0d40b2dc6

--- gui_engine/engine/font_mem.h
+++ gui_engine/engine/font_mem.h
@@ -51,6 +51,32 @@ typedef struct
     } font_mode_detail;
 } MEM_FONT_LIB;
 
+#pragma pack(1)
+typedef struct
+{
+    uint8_t head_length;
+    uint8_t file_type; //0x1 is font file
+    uint8_t version[4];
+    uint8_t font_size;
+    uint8_t rendor_mode;  // support 1 4 8
+    union
+    {
+        uint8_t value;
+        struct
+        {
+            uint8_t bold : 1;
+            uint8_t italic : 1;
+            uint8_t scanMode : 1;
+            uint8_t index_method : 1;   // 0 offset ; 1 address
+            uint8_t reserve : 4;
+        } detail;
+    } font_mode_detail;
+    uint32_t index_area_size;
+    uint8_t font_name_length;
+    // uint8_t* font_name;
+} GUI_FONT_HEAD;
+#pragma pack()
+
 /**
  * @brief Initialize the character binary file and store the font and
  * corresponding information in the font list
@@ -90,6 +116,25 @@ void gui_font_mem_draw(gui_text_t *text, gui_rect_t *rect);
  * @param text Widget pointer
  */
 void gui_font_mem_unload(gui_text_t *text);
+
+/**
+ * @brief Get the pixel width of the text in the current font file.
+ *
+ * @param content text pointer
+ * @param font_bin_addr font file address
+ * @param charset text encoding format
+ * @return uint32_t
+ */
+uint32_t gui_get_mem_char_width(void *content, void *font_bin_addr, TEXT_CHARSET charset);
+
+/**
+ * @brief Get the pixel width of the utf-8 text in the current font file.
+ *
+ * @param content text pointer
+ * @param font_bin_addr font file address
+ * @return uint32_t
+ */
+uint32_t gui_get_mem_utf8_char_width(void *content, void *font_bin_addr);
 
 #ifdef __cplusplus
 }
