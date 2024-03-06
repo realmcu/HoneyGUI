984f6b873a0aebd98cd37725fe099ee23b8b4ee6
xieyr <roy_xie@realsil.com.cn>
2024-03-06 22:31:43 +0800

    GUI: 480*480 demo add wifi speed tab
    [JIRA] BEE4BUG-179
    
    Change-Id: Ief5dcad9900beaa9d31a0faf36d365139013cfb7

--- gui_engine/example/screen_480_480/root/app/box/box480x480.js
+++ gui_engine/example/screen_480_480/root/app/box/box480x480.js
@@ -545,3 +545,33 @@ sw.getElementById('close_switch')
 sw.onOn(swCurtain)
 sw.onOff(swCurtain)
 
+textbox.getElementById('txt_wifi')
+textbox.write('Click')
+var textAnimateConfig = {
+    duration: 2000,
+    iterations:-1,
+    from: 1,
+    to: 0
+}
+var speed = 888;
+function textAnimateCallback(params) {
+    speed=speed + 1;
+//    textbox.write(wifi.readSpeed()+'bit/s')
+	textbox.write(speed.toString()+' bit/s')
+}
+
+function startSpeed(params) {
+textbox.setAnimate(textAnimateCallback, textAnimateConfig)
+textbox.palyAnimate()
+wifi.startSpeed()
+}
+function stopSpeed(params) {
+    textbox.setAnimate(textAnimateCallback, textAnimateConfig)
+    textbox.pauseAnimate()
+	wifi.stopSpeed()
+	textbox.write('Click')
+	speed = 0;
+    }
+sw.getElementById('sw_wifi')
+sw.onOn(startSpeed)
+sw.onOff(stopSpeed)
