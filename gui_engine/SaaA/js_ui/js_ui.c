1d20b9226617bd0b1957c33bda92df2ba459da96
xieyr <roy_xie@realsil.com.cn>
2024-03-18 11:07:56 +0800

    GUI: bugfix & update 480*320 widget
    Change-Id: I9c8da2cdce0bbf15c5a9d04dfbcafa2b7dce7773

--- gui_engine/SaaA/js_ui/js_ui.c
+++ gui_engine/SaaA/js_ui/js_ui.c
@@ -99,7 +99,7 @@ DECLARE_HANDLER(jump)
     if (obj)
     {
         // gui_log("jump %f\n", jerry_get_number_value(args[0]));
-        gui_tabview_jump_tab((void *)txtbox, jerry_get_number_value(args[0]), 0);
+        gui_tabview_jump_tab((void *)obj, jerry_get_number_value(args[0]), 0);
         gui_fb_change();
     }
     return jerry_create_undefined();
