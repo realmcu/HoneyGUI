104d6fe5908f9edef6cd10d88992214a35d773dc
jiaojiao_wang <jiaojiao_wang@apowertec.com>
2024-04-29 10:53:28 +0800

    GUI: gui_event.h code optimization adjustment
    [Jira]BTSOCGUI-251
    
    [JIRA TYPE] Bug-Native-Minor
    [Introduced by] NA
    
    [Issue description]
    gui_event.h code optimization adjustment
    
    [Issue reproduction]
    NA
    
    [Cause]
    NA
    
    [Solution]
    gui_event.h code optimization adjustment
    
    [Validation] NA
    
    Need Cherry Pick to:NA
    
    Change-Id: If990abd7d031a5633e04d92425655e65228a565a

--- gui_engine/widget/gui_event.h
+++ gui_engine/widget/gui_event.h
@@ -1,55 +1,88 @@
-/*
- * File      : guidef.h
- */
+/**
+ \internal
+*****************************************************************************************
+*     Copyright(c) 2017, Realtek Semiconductor Corporation. All rights reserved.
+*****************************************************************************************
+  * @file
+  * @brief
+  * @details
+  * @author
+  * @date
+  * @version
+  ***************************************************************************************
+    * @attention
+  * <h2><center>&copy; COPYRIGHT 2017 Realtek Semiconductor Corporation</center></h2>
+  ***************************************************************************************
+ \endinternal
+  */
+
+/*============================================================================*
+ *               Define to prevent recursive inclusion
+ *============================================================================*/
 #ifndef __GUI_EVENT_H__
 #define __GUI_EVENT_H__
 
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdio.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/*============================================================================*
+ *                        Header Files
+ *============================================================================*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+/** @defgroup WIDGET WIDGET
+  * @brief
+  * @{
+  */
+
+/*============================================================================*
+ *                         Types
+ *============================================================================*/
+/** @defgroup WIDGET_Exported_Types WIDGET Exported Types
+  * @brief
+  * @{
+  */
+
+/** @brief  ... */
 typedef enum
 {
-    GUI_EVENT_INVALIDE,
+    GUI_EVENT_INVALIDE                = 0x0000,
     /** Input device events*/
-    GUI_EVENT_TOUCH_PRESSED,
-    GUI_EVENT_TOUCH_PRESSING,
-    GUI_EVENT_TOUCH_CLICKED,
-    GUI_EVENT_TOUCH_LONG,
-    GUI_EVENT_TOUCH_RELEASED,
-    GUI_EVENT_TOUCH_TOUCH_RIGHT_SLIDE,
-    GUI_EVENT_TOUCH_TOUCH_LEFT_SLIDE,
+    GUI_EVENT_TOUCH_PRESSED           = 0x0001,
+    GUI_EVENT_TOUCH_PRESSING          = 0x0002,
+    GUI_EVENT_TOUCH_CLICKED           = 0x0003,
+    GUI_EVENT_TOUCH_LONG              = 0x0004,
+    GUI_EVENT_TOUCH_RELEASED          = 0x0005,
+    GUI_EVENT_TOUCH_TOUCH_RIGHT_SLIDE = 0x0006,
+    GUI_EVENT_TOUCH_TOUCH_LEFT_SLIDE  = 0x0007,
 
-    GUI_EVENT_KB_UP_PRESSED,
-    GUI_EVENT_KB_UP_RELEASED,
-    GUI_EVENT_KB_DOWN_PRESSED,
-    GUI_EVENT_KB_DOWN_RELEASED,
+    GUI_EVENT_KB_UP_PRESSED           = 0x0008,
+    GUI_EVENT_KB_UP_RELEASED          = 0x0009,
+    GUI_EVENT_KB_DOWN_PRESSED         = 0x000A,
+    GUI_EVENT_KB_DOWN_RELEASED        = 0x000B,
 
-    GUI_EVENT_KB_SHORT_CLICKED,
-    GUI_EVENT_KB_LONG_CLICKED,
+    GUI_EVENT_KB_SHORT_CLICKED        = 0x000C,
+    GUI_EVENT_KB_LONG_CLICKED         = 0x000D,
     /** Special events*/
     /** Other events*/
-    GUI_EVENT_1,
-    GUI_EVENT_2,
-    GUI_EVENT_3,
-    GUI_EVENT_4,
-    GUI_EVENT_5,
-    GUI_EVENT_6,
-    GUI_EVENT_7,
-    GUI_EVENT_8,
+    GUI_EVENT_1                       = 0x000E,
+    GUI_EVENT_2                       = 0x000F,
+    GUI_EVENT_3                       = 0x0010,
+    GUI_EVENT_4                       = 0x0011,
+    GUI_EVENT_5                       = 0x0012,
+    GUI_EVENT_6                       = 0x0013,
+    GUI_EVENT_7                       = 0x0014,
+    GUI_EVENT_8                       = 0x0015,
 
-    GUI_EVENT_DISPLAY_ON,
-    GUI_EVENT_DISPLAY_OFF,
-    GUI_EVENT_EXTERN_IO_JS,  // extern io event: route to js middle layer
+    GUI_EVENT_DISPLAY_ON              = 0x0016,
+    GUI_EVENT_DISPLAY_OFF             = 0x0017,
+    GUI_EVENT_EXTERN_IO_JS            = 0x0018,  // extern io event: route to js middle layer
 
-    GUI_EVENT_USER_DEFINE    = 0x100,
+    GUI_EVENT_USER_DEFINE             = 0x0100,
 } gui_event_t;
-
-
 
 typedef void (*gui_event_cb_t)(void *obj, gui_event_t e);
 
@@ -61,9 +94,12 @@ typedef struct _gui_event_dsc_t
     gui_event_t event_code;
 } gui_event_dsc_t;
 
+/** End of WIDGET_Exported_Types
+  * @}
+  */
+
 #ifdef __cplusplus
 }
 #endif
 
 #endif
-
