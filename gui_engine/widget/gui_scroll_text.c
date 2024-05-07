bb3a267000c848d76999f48add2fa40d44e9e0f0
luke_sun <luke_sun@realsil.com.cn>
2024-05-07 17:40:04 +0800

    GUI: fix scroll rext bug
    Change-Id: I3e53dd9bc82079416759ac5c309c4b11682df944

--- gui_engine/widget/gui_scroll_text.c
+++ gui_engine/widget/gui_scroll_text.c
@@ -249,6 +249,10 @@ static void gui_scroll_text_draw(gui_obj_t *obj)
     {
         cur_time_ms = gui_ms_get();
     }
+    draw_rect.x1 = text->base.offset_x;
+    draw_rect.y1 = text->base.offset_y;
+    draw_rect.x2 = draw_rect.x1 + obj->w;
+    draw_rect.y2 = draw_rect.y1 + obj->h;
 
     if (text->base.mode == SCROLL_X)
     {
@@ -277,13 +281,6 @@ static void gui_scroll_text_draw(gui_obj_t *obj)
             draw_rect.y1 = text->base.offset_y - text->cnt_value + text->start_value;
             draw_rect.y2 = draw_rect.y1 + offset;
         }
-    }
-    else
-    {
-        draw_rect.x1 = text->base.offset_x;
-        draw_rect.y1 = text->base.offset_y;
-        draw_rect.x2 = draw_rect.x1 + obj->w;
-        draw_rect.y2 = draw_rect.y1 + obj->h;
     }
 
     draw_rect.xboundleft = text->base.offset_x;
