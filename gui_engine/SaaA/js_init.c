6333be961d5dcfddc2ccccfd11b2b567b5cdc89e
sienna_shen <sienna_shen@realsil.com.cn>
2024-05-10 17:25:27 +0800

    GUI: fix SaaA js init error
    [JIRA] BTCUSTOMER-2402
    
    Change-Id: I582f38fd2c03ed8dd633e109392395a5b5039d9a

--- gui_engine/SaaA/js_init.c
+++ gui_engine/SaaA/js_init.c
@@ -17,7 +17,7 @@ static void *context_alloc(size_t size, void *cb_data_p)
     APP_PSRAM_SECTION static uint8_t js_buffer[(PKG_JMEM_HEAP_SIZE + 100) * 1024] = {0};
     return (void *)js_buffer;
 #elif defined RTL8762D
-    return (void *)(0x6900000);
+    static uint8_t *js_buffer = (void *)(0x6900000);
 
 #elif defined __WIN32
     static uint8_t js_buffer[(PKG_JMEM_HEAP_SIZE + 100) * 1024] = {0};
