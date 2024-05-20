dc886b2905ad7bb541be5e8741f4f537fd53a4eb
howie_wang <howie_wang@realsil.com.cn>
2024-05-20 14:45:55 +0800

    GUI: add todo def
    Change-Id: I08b355c83ba2fa64effefce420aa7d8f9634bb54

---
+++ gui_engine/core/def_list.h
@@ -0,0 +1,23 @@
+
+#ifndef __TODO_DEF_H__
+#define __TODO_DEF_H__
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
