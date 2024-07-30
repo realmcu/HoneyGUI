4aa1941e53862de905db7047762353f52b84ca65
howie_wang <howie_wang@realsil.com.cn>
2024-07-30 19:40:40 +0800

    GUI: lib script
    Change-Id: I628784e472b5823eec1265d80a7b61533a917927

--- lib/header_copy.bat
+++ lib/header_copy.bat
@@ -1,9 +1,17 @@
 @echo off
 
 rem 设置源目录和目标目录，可以是相对路径，也可以是绝对路径
+set "CURRENT_DIR=%~dp0"
 set "SOURCE_DIR=%~dp0..\gui_engine"
-set "TARGET_DIR=%~dp0output\include"
+set "TARGET_DIR=%~dp0..\..\ui_lib\inc"
 
+set "LIB_DIR=%~dp0build"
+
+
+if exist "%TARGET_DIR%" (
+    for /d %%D in ("%TARGET_DIR%\*") do rd /s /q "%%D"
+    del /q "%TARGET_DIR%\*"
+)
 rem 创建目标目录（如果不存在的话）
 if not exist "%TARGET_DIR%" (
     mkdir "%TARGET_DIR%"
@@ -11,5 +19,10 @@ if not exist "%TARGET_DIR%" (
 
 rem 拷贝头文件到目标目录
 xcopy "%SOURCE_DIR%\*.h" "%TARGET_DIR%\" /S /E /Y /I
+
+xcopy "%LIB_DIR%\*.lib" "%TARGET_DIR%\..\"
+
+xcopy "%CURRENT_DIR%\script\SConscript" "%TARGET_DIR%\..\"
+
 
 exit /b %ERRORLEVEL%
