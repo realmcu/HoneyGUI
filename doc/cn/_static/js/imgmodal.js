3b6da2af1a2568a910887b9c6e115e4fbd1cf9f0
sienna_shen <sienna_shen@realsil.com.cn>
2025-04-15 10:19:47 +0800

    GUI: fix doc style
    Change-Id: I31903a277deb8ba1873a85f68b92e92326a57d2c

--- doc/cn/_static/js/imgmodal.js
+++ doc/cn/_static/js/imgmodal.js
@@ -1,32 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var imgModal = document.getElementById("imgModal");
-    var imgInModal= document.getElementById("imgInModal");
-    var imgModalCloseButton = document.getElementsByClassName("img-modal-close")[0];
-    var curImgScale = 1;
- 
+    const imgModal = document.getElementById("imgModal");
+    const imgInModal= document.getElementById("imgInModal");
+    const imgModalCloseButton = document.getElementsByClassName("img-modal-close")[0];
+    // const wyGridContainer = document.querySelector(".wy-grid-for-nav");
+    let curImgScale = 1;
+
     function addImgClickListener() {
         const initModalImgSize = function() {
-            const innerWidth = window.innerWidth;
-            const innerHeight = window.innerHeight;
-        
-            imgInModal.style.maxWidth = `${innerWidth}px`;
-            imgInModal.style.maxHeight = `${innerHeight}px`;
-            
-            imgInModal.style.transformOrigin = "center center";
-            imgInModal.style.transform = 'scale(1)'; // Reset scale and translation on each open
             curImgScale = 1; // Reset scale value
+            updateImgTransform(1);
         }
- 
+
         const handleImgClick = function() { // 改为普通的函数表达式
-            imgModal.style.display = "flex";
-            imgModal.style.justifyContent = 'center';
-            imgModal.style.alignItems = 'center';
-    
             imgInModal.src = this.src; // 这里 this 的引用会指向被点击的 img 元素
+            imgModal.style.display = "block";
             initModalImgSize();
             document.documentElement.style.overflow = "hidden"; // 禁用滚动
+            // wyGridContainer.classList.add("prevent-manipulation");
         };
- 
+
         document.querySelectorAll('.rst-content img').forEach(function(imgItem) {
             var validImgRegex = /\.(jpe?g|png|gif|bmp|webp)$/i;
             var parentElement = imgItem.parentElement;
@@ -51,25 +43,39 @@ document.addEventListener("DOMContentLoaded", function () {
             if (event.target === imgModal || event.target === imgModalCloseButton) {
                 imgModal.style.display = "none";
                 document.documentElement.style.overflow = "auto"; // 启用页面滚动
+                // wyGridContainer.classList.remove("prevent-manipulation");
             }
         });
     }
- 
-    function addImgZoomHandler() {
-        const updateImgModalPositionStyle = function() {
-            const hasHorScrollBar = (imgInModal.clientWidth * curImgScale) > imgModal.clientWidth;
-            const hasVerScrollBar = (imgInModal.clientHeight * curImgScale) > imgModal.clientHeight;
-    
-            // set the horizontal alignment
-            imgModal.style.justifyContent = hasHorScrollBar ? 'start' : 'center';
-            // set the vertical alignment
-            imgModal.style.alignItems = hasVerScrollBar ? 'start' : 'center';
-            // set transform origin attribute: start position
-            const horTransformOrigin = hasHorScrollBar ? 'left' : 'center';
-            const verTransformOrigin = hasVerScrollBar ? 'top' : 'center';
-            imgInModal.style.transformOrigin = `${horTransformOrigin} ${verTransformOrigin}`;
+
+    function updateImgTransform(scale) {
+        let imgModalWid = imgModal.offsetWidth;
+        let imgModalHei = imgModal.offsetHeight;
+        let imgInModalWid= imgInModal.scrollWidth * scale;
+        let imgInModalHei = imgInModal.scrollHeight * scale;
+
+        const offsetX = (imgModalWid - imgInModalWid)/2;
+        const offsetY = (imgModalHei - imgInModalHei)/2;
+
+        let translateX = 0;
+        let translateY = 0;
+        if(offsetX > 0) {
+            translateX = offsetX;
         }
- 
+        else {
+            translateX = 0;
+        }
+        if(offsetY > 0) {
+            translateY = offsetY;
+        }
+        else {
+            translateY = 0;
+        }
+
+        imgInModal.style.transform = `translate(${translateX}px, ${translateY}px) scale(${scale})`;
+    }
+
+    function addImgZoomHandler() {
         // image zoom in/out
         imgInModal.addEventListener("wheel", function(event) {
             event.preventDefault();
@@ -80,12 +86,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Scroll down (zoom out)
                 curImgScale = Math.max(0.7, curImgScale - 0.06); // Avoid scaling below the original size
             }
- 
-            imgInModal.style.transform = `scale(${curImgScale})`;
-            updateImgModalPositionStyle();
+
+            updateImgTransform(curImgScale);
         });
     }
- 
+
     function addImgTouchEventHandler() {
         let initialDistance = 0;
     
@@ -114,21 +119,21 @@ document.addEventListener("DOMContentLoaded", function () {
                     { x: touch2.pageX, y: touch2.pageY }
                 );
                 const scaleChange = newDistance / initialDistance;
+                if(scaleChange >= 1) {
+                    curImgScale = Math.min(3, curImgScale + 0.06); // zoom in
+                } else {
+                    curImgScale = Math.max(0.7, curImgScale - 0.06); // zoom out
+                }
     
-                // Calculate new scale and constrain it between 0.7 and 3
-                curImgScale *= scaleChange;
-                curImgScale = Math.max(0.7, Math.min(curImgScale, 3));
-    
-                // Apply new scale
-                imgInModal.style.transform = `scale(${curImgScale})`;
-                updateImgModalPositionStyle();
+                // Apply new transform
+                updateImgTransform(curImgScale);
     
                 // Update initial distance for the next move event
                 initialDistance = newDistance;
             }
         }, { passive: false });
     }
- 
+
     addImgClickListener();
     addImgModalCloseHandler();
     addImgZoomHandler();
--- doc/en/_static/js/glossary.js
+++ doc/en/_static/js/glossary.js
@@ -1,35 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     let lastHighlightedElement = null;
- 
+
     function highlightElement() {
         // 清除之前的高亮效果
         if (lastHighlightedElement) {
-            lastHighlightedElement.classList.remove('glossary-highlighted');
+            lastHighlightedElement.classList.remove('dt-highlighted');
             lastHighlightedElement = null;
         }
- 
+
         // 获取当前URL中的锚点（即#后面的部分）
         const anchor = window.location.hash;
- 
+
         // 判断是否存在锚点
         if (anchor) {
             // 转义id中可能存在的 . 字符以便于使用 querySelector
             const safeAnchor = anchor.replace(/\./g, '\\.');
-            const element = document.querySelector(safeAnchor);
+            const element = document.querySelector(`dt${safeAnchor}`);
             
             // 如果找到了该元素，并且元素标签为 dt
-            if (element && element.tagName.toUpperCase() == 'DT') {
+            if (element) {
                 // 给该元素添加highlighted class
-                element.classList.add('glossary-highlighted');
+                element.classList.add('dt-highlighted');
                 // 缓存该元素，避免重复操作
                 lastHighlightedElement = element;
             }
         }
     }
- 
+
     // 初始化高亮效果
     highlightElement();
- 
+
     // 监听hashchange事件，只在hash变化时执行高亮操作
     window.addEventListener('hashchange', highlightElement);
 });
