ccb8d25512b9a83128b92924b721431c41d2a87a
miaomiao_wu <miaomiao_wu@realsil.com.cn>
2025-04-16 15:18:03 +0800

    GUI: save rst src after release
    Change-Id: Id10b66a19d26f55a40e294058ac197ca8a47effb

--- doc/conf_common.py
+++ doc/conf_common.py
@@ -8,6 +8,8 @@
 
 import os
 import sys
+import re
+import json
 from datetime import datetime
 from docutils import nodes
 from docutils.parsers.rst import roles
@@ -23,6 +25,7 @@ project = 'RTKIOT GUI'
 author = 'RTKIOT GUI'
 release = 'v0.0.0.1'
 
+link_data = dict()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -201,6 +204,47 @@ def custom_role(role_name, rawtext, text, lineno, inliner, options={}, content=[
     node = nodes.inline(rawtext, text, classes=[role_name])
     return [node], []
 
+def process_rst_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    figure_pattern = re.compile(r'^[ \t]*\.{1,2}\s+(figure|image)::[^\n]*\n(?:^[ \t]+:[^:]+:.*\n?)*', re.MULTILINE)
+    processed_content = re.sub(figure_pattern, '', content)
+
+    return processed_content
+
+
+def record_link(app, pagename, templatename, context, doctree):
+    # get uri
+    uri = app.builder.get_target_uri(pagename)
+    link_data[pagename] = uri
+
+    if os.getenv('COPY_RST_FILES', 'false').lower() == 'true':
+        source_dir = app.srcdir
+        target_base_dir = os.path.join(app.outdir, "doc_source")
+
+        for suffix in source_suffix:
+            rst_file_path = os.path.join(source_dir, pagename + suffix)
+            
+            if os.path.exists(rst_file_path):
+                modified_content = process_rst_file(rst_file_path)
+                target_dir = os.path.join(target_base_dir, os.path.dirname(pagename))
+                os.makedirs(target_dir, exist_ok=True)
+                
+                target_file_path = os.path.join(target_dir, os.path.basename(rst_file_path))
+                with open(target_file_path, 'w', encoding='utf-8') as target_file:
+                    target_file.write(modified_content)
+                break 
+
+def write_json(app, exception):
+    output_file = os.path.join(app.outdir, 'link_map.json')
+    try:
+        with open(output_file, 'w') as f:
+            json.dump(link_data, f, indent=4)
+        print(f"Successfully wrote URL map to {output_file}")
+    except Exception as e:
+        print(f"Error writing URL map: {e}")
+
 custom_role_list = ['red-text', 'bolditalic']
 
 # Example configuration for intersphinx: refer to the Python standard library.
@@ -218,5 +262,8 @@ def setup(app):
     # register custom class role
     for role in custom_role_list:
         app.add_role(role, custom_role)
+    # listen html-page-context event, record html link
+    app.connect('html-page-context', record_link)
+    app.connect('build-finished', write_json)
     # app.add_css_file('css/customdoxygen.css')
     # app.add_css_file('css/my_customdoxygen.css')
