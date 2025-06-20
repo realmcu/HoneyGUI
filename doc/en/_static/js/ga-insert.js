3d7566e4a3cf993276cec1d892b03e05025972c2
miaomiao_wu <miaomiao_wu@realsil.com.cn>
2025-06-20 11:34:20 +0800

    GUI: add google analytics
    Change-Id: I00281122d484ea6d87880bfbce49a0c14e5135e8

---
+++ doc/en/_static/js/ga-insert.js
@@ -0,0 +1,13 @@
+(function() {
+    const gaId = window.GA_CONFIG?.id || "G-8MDP27F36Q";
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', gaId);
+
+})();
