31b2597b0e56272285c08acffbee0329c93e2644
miaomiao_wu <miaomiao_wu@realsil.com.cn>
2025-08-14 10:39:21 +0800

    GUI: add rst check config file
    Change-Id: I2633665c5251d94a9a9796701132037c4e99b3ae

---
+++ doc/script/rst_check_config.py
@@ -0,0 +1,28 @@
+
+media_check_exclude_pattern = [
+    "Glossary.rst"
+]
+
+global_check_exclude_pattern = [
+    "Glossary.rst"
+]
+
+basic_check_exclude_pattern = [
+    "Glossary.rst"
+]
+
+title_check_exclude_pattern = [
+    "Glossary.rst"
+]
+
+general_exclude_pattern = [
+    "Glossary.rst"
+]
+
+glossary_check_exclude_pattern = [
+
+]
+
+ref_check_exclude_pattern = [
+    
+]
