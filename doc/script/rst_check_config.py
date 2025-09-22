8411903fa6c1cbfdd6c193141bc21a19b8b415e5
miaomiao_wu <miaomiao_wu@realsil.com.cn>
2025-09-22 18:05:39 +0800

    Script-Other: add title check bypass config
    Change-Id: Ibcae006a1a956af8536786e690aa045fb5415dac

--- doc/script/rst_check_config.py
+++ doc/script/rst_check_config.py
@@ -15,16 +15,23 @@ title_check_exclude_pattern = [
     "Glossary.rst"
 ]
 
+# A global list of titles that bypass all other title validation rules.
+global_title_bypass = []
+
+# A global list of files that bypass all validation rules.
 general_exclude_pattern = [
     "Glossary.rst",
     "release_notes/sdk_release_notes.rst",
     "widgets/gui_window.rst"
 ]
 
+
+# A global list of glossary that bypass glossry check rule.
 glossary_check_exclude_pattern = [
 
 ]
 
+# A global list of url links that bypass link validation check rule.
 ref_check_exclude_pattern = [
     
 ]
