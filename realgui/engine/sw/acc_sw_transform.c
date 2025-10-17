b41f8299ebe2559da466af29fcd2c987bde617f6
astor_zhang <astor_zhang@realsil.com.cn>
2025-10-17 15:46:37 +0800

    GUI: fix boundary issue when doing AA
    Change-Id: I096fd7879404b0789c3eef205581cde7d0d34d52

--- realgui/engine/sw/acc_sw_transform.c
+++ realgui/engine/sw/acc_sw_transform.c
@@ -36,6 +36,7 @@ static uint16_t do_blending_acc_2_rgb565_opacity(uint32_t fg, uint32_t bg, uint8
     return (uint16_t)((result >> 16) | result);
 }
 
+#ifdef HONEYGUI_4XAA
 static color_argb8565_value_t do_average_acc_2_argb8565_opacity(uint32_t fg, uint32_t bg,
                                                                 uint8_t alpha_fg, uint8_t alpha_bg, uint8_t ratio)
 {
@@ -51,6 +52,27 @@ static color_argb8565_value_t do_average_acc_2_argb8565_opacity(uint32_t fg, uin
     uint32_t result = (((((fg - bg) * rgb_ratio) >> 5) + bg) & 0x07e0f81f);
     color_argb8565_value_t pixel = {.rgb = (uint16_t)((result >> 16) | result), .a = ((alpha_fg * ratio + alpha_bg * (255 - ratio)) >> 8)};
     return pixel;
+}
+#endif
+
+static uint16_t do_average_blend_acc_2_argb8565_opacity(uint32_t fg, uint32_t bg, uint32_t write,
+                                                        uint8_t alpha_fg, uint8_t alpha_bg, uint8_t ratio)
+{
+    // Alpha converted from [0..255] to [0..31]
+    // Converts  0000000000000000rrrrrggggggbbbbb
+    //     into  00000gggggg00000rrrrr000000bbbbb
+    // with mask 00000111111000001111100000011111
+    // This is useful because it makes space for a parallel fixed-point multiply
+
+    uint8_t rgb_ratio = (ratio + 4) >> 3;
+    bg = ((bg | (bg << 16)) & 0x07e0f81f);
+    fg = ((fg | (fg << 16)) & 0x07e0f81f);
+    uint32_t result = (((((fg - bg) * rgb_ratio) >> 5) + bg) & 0x07e0f81f);
+    uint8_t alpha = (alpha_fg * ratio + alpha_bg * (255 - ratio)) >> 8;
+    alpha = (alpha + 4) >> 3;
+    write = ((write | (write << 16)) & 0x07e0f81f);
+    result = (((((result - write) * alpha) >> 5) + write) & 0x07e0f81f);
+    return (uint16_t)((result >> 16) | result);
 }
 
 void sw_transform_for_rgb565(draw_img_t *image, gui_dispdev_t *dc,
@@ -227,7 +249,13 @@ void sw_transform_for_argb8565_aa(draw_img_t *image, gui_dispdev_t *dc,
             int x = (int)X;
             int y = (int)Y;
 
-            if ((x >= source_w) || (x < -1) || (y < -1) || (y >= source_h))
+            if ((x >= source_w) || (x < -1) ||
+#ifdef HONEYGUI_4XAA
+                (y < -1)
+#else
+                (y < 0)
+#endif
+                || (y >= source_h))
             {
                 X += m00;
                 Y += m10;
@@ -242,17 +270,24 @@ void sw_transform_for_argb8565_aa(draw_img_t *image, gui_dispdev_t *dc,
             int write_off = write_offset + j;
             int image_off = y * source_w + x;
 
+#ifdef HONEYGUI_4XAA
+            if (x >= 0 && y >= 0)
+#else
             if (x >= 0)
+#endif
             {
                 pixel1 = *((color_argb8565_value_t *)(uintptr_t)image_base + image_off);
             }
 #ifdef HONEYGUI_4XAA
-            if (y >= 0)
+            if (y >= -1)
             {
                 pixel3 = *((color_argb8565_value_t *)(uintptr_t)image_base + image_off + source_w);
             }
-#endif
+
+            if (x < source_w - 1 && y >= 0)
+#else
             if (x < source_w - 1)
+#endif
             {
                 pixel2 = *((color_argb8565_value_t *)(uintptr_t)image_base + image_off + 1);
             }
@@ -262,12 +297,15 @@ void sw_transform_for_argb8565_aa(draw_img_t *image, gui_dispdev_t *dc,
                 pixel4 = *((color_argb8565_value_t *)(uintptr_t)image_base + image_off + source_w + 1);
             }
 #endif
-            pixel2 = do_average_acc_2_argb8565_opacity(pixel1.rgb, pixel2.rgb, pixel1.a, pixel2.a, xRatio);
 #ifdef HONEYGUI_4XAA
+            pixel2 = do_average_acc_2_argb8565_opacity(pixel1.rgb, pixel2.rgb, pixel1.a, pixel2.a, xRatio);
             pixel4 = do_average_acc_2_argb8565_opacity(pixel3.rgb, pixel4.rgb, pixel3.a, pixel4.a, xRatio);
-            pixel2 = do_average_acc_2_argb8565_opacity(pixel2.rgb, pixel4.rgb, pixel2.a, pixel4.a, yRatio);
+            writebuf[write_off] = do_average_blend_acc_2_argb8565_opacity(pixel2.rgb, pixel4.rgb,
+                                                                          writebuf[write_off], pixel2.a, pixel4.a, yRatio);
+#else
+            writebuf[write_off] = do_average_blend_acc_2_argb8565_opacity(pixel1.rgb, pixel2.rgb,
+                                                                          writebuf[write_off], pixel1.a, pixel2.a, xRatio);
 #endif
-            writebuf[write_off] = do_blending_acc_2_rgb565_opacity(pixel1.rgb, writebuf[write_off], pixel1.a);
             X += m00;
             Y += m10;
         }
