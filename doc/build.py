1921e67dcb6cbce6fb235c495fa810dc4d84291a
miaomiao_wu <miaomiao_wu@realsil.com.cn>
2025-12-03 14:54:45 +0800

    GUI: update tool match pattern
    Change-Id: I661d42c30187007a46c54245a68ca85294c51c24

--- doc/build.py
+++ doc/build.py
@@ -58,7 +58,7 @@ Font Convert Tool: :download:`Font Convert Tool <../{}>`
 RVisualDesigner Tool: :download:`RVisualDesigner Tool <../{}>`
 
 '''.format(download_tool_list[0], download_tool_list[1], download_tool_list[2])
-    stream = re.sub(r'(.. Add tool downlod link here\n)', lambda objs: objs.group(1) + download_link, stream, count=0, flags=re.M)
+    stream = re.sub(r'(.. Add tool downlod link here)', lambda objs: objs.group(1) + "\r\n" + download_link, stream, count=0, flags=re.M)
   with open(index_file, mode='w+', newline='', errors='surrogateescape') as fd:
     fd.write(stream)
 
