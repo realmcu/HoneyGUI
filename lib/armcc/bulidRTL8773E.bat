670bf87276e3af1de5886483caf28f5e85e69f50
wenjing_jiang <wenjing_jiang@realsil.com.cn>
2025-12-17 19:11:13 +0800

    GUI: update gui lib cmake
    Change-Id: If621c397231f82a2803c4ab1311f1a309d748b7d

--- lib/armcc/bulidRTL8773E.bat
+++ lib/armcc/bulidRTL8773E.bat
@@ -1,5 +1,5 @@
 
-cmake -G "MinGW Makefiles" -DSOC=RTL8773E -DCONFIG_REALTEK_BUILD_LITE3D=y -B "./temp"
+cmake -G "MinGW Makefiles" -DSOC=RTL8773E -B "./temp"
 cd temp
 cmake --build . -j 32
 cmake --build . --target install -j 32
