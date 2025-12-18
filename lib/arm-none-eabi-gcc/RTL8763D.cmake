20a5efa427de60ee39dd157aaf6b731fef97097f
wenjing_jiang <wenjing_jiang@realsil.com.cn>
2025-12-18 17:29:26 +0800

    GUI: update gui lib cmake
    Change-Id: Ic16d1b561f762966ab1e5bf2affc2a0762fd8976

---
+++ lib/arm-none-eabi-gcc/RTL8763D.cmake
@@ -0,0 +1,41 @@
+set(CONFIG_SOC_SERIES_RTL8763D y)
+set(mcpu_arch "-mcpu=cortex-m4")
+	set(mfpu_arch "-mfpu=fpv4-sp-d16")
+	set(mfloat_arch "-mfloat-abi=hard")
+	set(march_arch "")
+	add_compile_options(
+						-w
+						-g1
+						-Os
+						-mcpu=cortex-m4
+						-mfpu=fpv4-sp-d16
+						-mfloat-abi=hard
+						-mthumb -Wall
+						-Wno-parentheses
+						-Wno-unused-variable
+						-Wno-return-type
+						-Wno-address-of-packed-member
+						-fdata-sections
+						-ffunction-sections
+						-std=gnu99
+						-fstack-usage
+						-fno-delete-null-pointer-checks
+						-fshort-wchar
+                        -Werror=undef 
+                        -Werror=sign-compare 
+                        -Werror=unused-parameter 
+                        -Werror=missing-braces 
+                        -Werror=unused-variable 
+                        -Werror=format 
+                        -Werror=char-subscripts 
+                        -Werror=parentheses 
+                        -Werror=implicit-fallthrough 
+                        -Werror=type-limits 
+                        -Wno-packed 
+                        -Wno-missing-prototypes 
+                        -Wno-missing-noreturn 
+                        -Wno-sign-conversion 
+                        -Wno-unused-macros 
+                        -Wno-implicit-function-declaration
+                        -Werror=deprecated-copy
+                        -Wmissing-field-initializers)
---
+++ lib/arm-none-eabi-gcc/bulidRTL8763D.bat
@@ -0,0 +1,6 @@
+
+cmake -G "MinGW Makefiles" -DSOC=RTL8763D -B "./temp"
+cd temp
+cmake --build . -j 32
+cmake --build . --target install -j 32
+pause
