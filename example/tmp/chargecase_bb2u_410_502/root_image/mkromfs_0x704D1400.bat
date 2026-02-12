1a31e597360109b788ef44385ea8dc5ea6c341d0
howie_wang <howie_wang@realsil.com.cn>
2026-02-12 11:16:22 +0800

    GUI: remove chargecase_bb2u_410_502 demo and config
    - Remove example/tmp/chargecase_bb2u_410_502 directory
    - Remove REALTEK_BUILD_CHARGECASE_410_502_DEMO from Kconfig
    
    Change-Id: I3817300aa1f86182cde1e1a5765e86c2e45f1c4a

--- example/tmp/chargecase_bb2u_410_502/root_image/mkromfs_0x704D1400.bat
+++
@@ -1,3 +0,0 @@
-python ..\..\..\..\tool\mkromfs\mkromfs_for_honeygui.py -i root -o root_0x704D1400.bin -b -a 0x704D1400
-
-srec_cat root_0x00950000.bin -binary -offset 0x704D1400 -o root_0x704D1400.hex -intel
