80ff48b686c2a2318df23586f18c8b0976d9ce8a
tracy_yan <tracy_yan@realsil.com.cn>
2026-05-25 17:30:04 +0800

    Script-CI: enable illegal words check
    Change-Id: I4778691698ae81d163734d251efefb747ccc4f46

---
+++ script/illegal_check_config.py
@@ -0,0 +1,13 @@
+check_base_path = r""
+
+apache_path = []
+
+mit_path = []
+
+ignore_declaration_check = [
+    "", #""means skip all
+]
+
+ignore_illgeal_check = [
+    "script/illegal_check_config.py",
+]
