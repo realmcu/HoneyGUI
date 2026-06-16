01a3e6df507279edefc170be9aaeffb945057786
luke_sun <luke_sun@realsil.com.cn>
2026-06-16 14:20:09 +0800

    GUI: update Copyright
    Change-Id: I80abff140239cec2b78f537daed0ed8efdecc4d7

--- realgui/server/gui_api_audio.c
+++ realgui/server/gui_api_audio.c
@@ -1,18 +1,8 @@
-/**
-*****************************************************************************************
-*     Copyright(c) 2025, Realtek Semiconductor Corporation. All rights reserved.
-*****************************************************************************************
-  * @file gui_api_audio.c
-  * @brief Application Programming Interface for UI
-  * @details Display Device
-  * @author roy@realsil.com.cn
-  * @date 2026/01/27
-  * @version 1.0
-  ***************************************************************************************
-    * @attention
-  * <h2><center>&copy; COPYRIGHT 2025 Realtek Semiconductor Corporation</center></h2>
-  ***************************************************************************************
-  */
+/*
+ * Copyright (c) 2026, Realtek Semiconductor Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 
 /*============================================================================*
